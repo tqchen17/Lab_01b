a600f25c6a6f324e0933e88c54be4c60101a0589
tqchen17 <tqchen17@gmail.com>
2023-02-06 15:11:03 -0500

    2/6/2023: lab 01b changes - changing background and text color

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button incrementButton;
     Button decrementButton;
+    Switch switchMode;
+    ConstraintLayout background;
     TextView greetingDisplay;
     TextView num;
     int count = 0;
@@ -23,6 +29,33 @@ public class MainActivity extends AppCompatActivity {
         incrementButton = findViewById(R.id.incrementButton);
         decrementButton = findViewById(R.id.decrementButton);
         greetingDisplay = findViewById(R.id.textView);
+        switchMode = findViewById(R.id.night);
+        background = findViewById(R.id.background);
+        if (switchMode.isChecked()) {
+            switchMode.setText("Click on the switch for a new message");
+            switchMode.setTextColor(Color.argb(255, 255, 255, 255));
+            background.setBackgroundColor(Color.argb(255, 0, 0, 0));
+        } else {
+            switchMode.setText("Fun mode");
+            switchMode.setTextColor(Color.argb(255, 0, 0, 0));
+            background.setBackgroundColor(Color.argb(255, 255, 255, 255));
+        }
+
+        switchMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    switchMode.setText("Click on the switch for a new message");
+                    switchMode.setTextColor(Color.argb(255, 255, 255, 255));
+                    background.setBackgroundColor(Color.argb(255, 0, 0, 0));
+                } else {
+                    switchMode.setText("Fun mode");
+                    switchMode.setTextColor(Color.argb(255, 0, 0, 0));
+                    background.setBackgroundColor(Color.argb(255, 255, 255, 255));
+                }
+            }
+        });
+
         num = findViewById(R.id.number);
         incrementButton.setOnClickListener(new View.OnClickListener() {
             @Override
