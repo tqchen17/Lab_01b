e78a5591aacaaa1b10536f9a0bb926452856e1f5
tqchen17 <tqchen17@gmail.com>
2023-02-10 10:46:46 -0500

    2/10/2023: lab 01b changes - changing greeting message after every switch and creating a shape for inc/dec buttons

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,6 +12,9 @@ import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     Button incrementButton;
     Button decrementButton;
@@ -19,26 +22,36 @@ public class MainActivity extends AppCompatActivity {
     ConstraintLayout background;
     TextView greetingDisplay;
     TextView num;
-    int count = 0;
+    List<String> l = new ArrayList<>();
+    int incCount = 0;
+    int loop = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView textView = new TextView(this);
-        textView.setText(R.string.greet_dad);
+        l.add("Nice to meet ya, Tommy Chen!");
+        l.add("Hello there");
+        l.add("Bonjour AmithP");
+        l.add("Happy Birthday Pranav!");
+        l.add("2024tchen, it's a pleasure to make your acquaintance.");
+        l.add("Hi, Mr. Tra!");
         setContentView(R.layout.activity_main);
         incrementButton = findViewById(R.id.incrementButton);
         decrementButton = findViewById(R.id.decrementButton);
         greetingDisplay = findViewById(R.id.textView);
+        num = findViewById(R.id.number);
         switchMode = findViewById(R.id.night);
         background = findViewById(R.id.background);
         if (switchMode.isChecked()) {
             switchMode.setText("Click on the switch for a new message");
             switchMode.setTextColor(Color.argb(255, 255, 255, 255));
             background.setBackgroundColor(Color.argb(255, 0, 0, 0));
+            num.setTextColor(Color.argb(255, 255, 255, 255));
         } else {
             switchMode.setText("Fun mode");
             switchMode.setTextColor(Color.argb(255, 0, 0, 0));
             background.setBackgroundColor(Color.argb(255, 255, 255, 255));
+            num.setTextColor(Color.argb(255, 0, 0, 0));
+
         }
 
         switchMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -48,10 +61,16 @@ public class MainActivity extends AppCompatActivity {
                     switchMode.setText("Click on the switch for a new message");
                     switchMode.setTextColor(Color.argb(255, 255, 255, 255));
                     background.setBackgroundColor(Color.argb(255, 0, 0, 0));
+                    num.setTextColor(Color.argb(255, 255, 255, 255));
+
                 } else {
                     switchMode.setText("Fun mode");
                     switchMode.setTextColor(Color.argb(255, 0, 0, 0));
                     background.setBackgroundColor(Color.argb(255, 255, 255, 255));
+                    loop++;
+                    if(loop >= l.size()) { loop = 0; }
+                    greetingDisplay.setText(l.get(loop));
+                    num.setTextColor(Color.argb(255, 0, 0, 0));
                 }
             }
         });
@@ -60,15 +79,15 @@ public class MainActivity extends AppCompatActivity {
         incrementButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                System.out.print("incrementing: " + ++count);
-                num.setText(""+count);
+                System.out.print("incrementing: " + ++incCount);
+                num.setText(""+incCount);
             }
         });
         decrementButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                System.out.print("decrementing: " + --count);
-                num.setText(""+count);
+                System.out.print("decrementing: " + --incCount);
+                num.setText(""+incCount);
             }
         });
 
